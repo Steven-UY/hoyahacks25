038673309239592cfb536929657500807c2ef326
Steven Uy <81347262+Steven-UY@users.noreply.github.com>
2025-01-25 20:59:00 -0500

    I'm not even sure

--- PharmaSee_Frontend/src/app/page.js
+++ PharmaSee_Frontend/src/app/page.js
@@ -1,11 +1,5 @@
-<<<<<<< Updated upstream
-import SignUpForm from '../components/SignUpForm'
-import LoginForm from '@/components/LoginForm'
-import PatientDashboard from '@/components/PatientDashboard'
-=======
 import SignUpForm from '@/components/SignUpForm'
 
->>>>>>> Stashed changes
 
 export default function SignUpPage() {
   return (
