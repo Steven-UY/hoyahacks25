c76ccb4999f45eb5169211b923dc22f5ab75c285
Chris Jin <cj0711888@gmail.com>
2025-01-25 21:20:49 -0500

    edited page.js??

--- PharmaSee_Frontend/src/app/page.js
+++ PharmaSee_Frontend/src/app/page.js
@@ -1,11 +1,6 @@
-<<<<<<< Updated upstream
 import SignUpForm from '../components/SignUpForm'
 import LoginForm from '@/components/LoginForm'
 import PatientDashboard from '@/components/PatientDashboard'
-=======
-import SignUpForm from '@/components/SignUpForm'
-
->>>>>>> Stashed changes
 
 export default function SignUpPage() {
   return (
